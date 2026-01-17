5cc3091c751281a010b26b4c362e7aa334c28c20
Carlos Ramos <carlosramosdev@protonmail.com>
2026-01-17 16:51:56 +0100

    Implemented MainScreen

--- app/src/main/java/com/cr_d/canva_app/MainActivity.kt
+++ app/src/main/java/com/cr_d/canva_app/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.cr_d.canva_app.screens.MainScreen
 import com.cr_d.canva_app.ui.theme.Canva_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Canva_appTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(innerPadding)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Canva_appTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/cr_d/canva_app/screens/MainScreen.kt
@@ -0,0 +1,12 @@
+package com.cr_d.canva_app.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(innerPadding: PaddingValues){
+    Box(){
+
+    }
+}
